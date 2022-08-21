6d13c0cfd455c169974e690fda59fb7ad0010066
Ahmed Kayed <Ahmed@Kayed.com>
2022-08-21 20:04:39 +0200

    Third_Commit

--- src/app/components/confirmation/confirmation.component.ts
+++ src/app/components/confirmation/confirmation.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-confirmation',
   templateUrl: './confirmation.component.html',
   styleUrls: ['./confirmation.component.css']
 })
 export class ConfirmationComponent implements OnInit {
-  NumberOfItems:number=0;
-  PriceOfItems:number=0;
+  numberOfItems:number=0;
+  priceOfItems:number=0;
 
-  constructor(private CartService:CartService) { }
+  constructor(private cartService:CartService, private router:Router) { }
 
   ngOnInit(): void {
-    this.NumberOfItems=this.CartService.numberOfItemsInCart();
-    this.PriceOfItems=this.CartService.cartTotal();
-    this.CartService.clearCart();
+    this.numberOfItems=this.cartService.numberOfItemsInCart();
+    this.priceOfItems=this.cartService.cartTotal();
+    this.cartService.clearCart();
+    
+    if(this.numberOfItems===0){
+      //If the user typed the url/confirmation
+      this.router.navigateByUrl('/');
+    }
   }
 
 }
